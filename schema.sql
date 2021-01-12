5b3904f038f6fa89b7c9bf6d75da16c2d2871c8d
Elizabeth Leshuk <eleshuk@gmail.com>
2021-01-11 19:02:16 -0800

    reloading all files into new version of the repo without large files

---
+++ schema.sql
@@ -0,0 +1,33 @@
+-- create spotify table
+CREATE TABLE "spotify" (
+	name VARCHAR NOT NULL PRIMARY KEY,
+	album VARCHAR NOT NULL, 
+	artists VARCHAR NOT NULL,
+	explicit BOOLEAN, 
+	valence DECIMAL,
+	tempo DECIMAL,
+	year DATE NOT NULL,
+	release_date DATE NOT NULL,
+	duration_sec DECIMAL,
+	duration_min DECIMAL
+);
+
+-- if necessary
+-- DROP TABLE spotify;
+
+
+-- create billboard table
+CREATE TABLE "billboard" (
+	id INT,
+	name VARCHAR NOT NULL PRIMARY KEY,
+	artist VARCHAR NOT NULL,
+	current_rank INT,
+	peak_rank INT,
+	weeks_on_chart INT
+);
+
+SELECT * FROM billboard;
+
+-- if necessary
+DROP TABLE billboard;
+
